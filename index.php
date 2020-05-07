3d29d0c35f3d6d60384aa62d9e8c687fbf3a6c9e
Kaniyama_t <webmaster@kaniyama.net>
2020-05-07 20:23:34 +0900

    index.phpに何も表示されないバグを修正

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
         <title>情シス1 授業課題デプロイ場所</title>
     </head>
     <h1>情シス1 授業課題デプロイ場所</h1>
+    <hr />
+    <h3>Github Repository</h3>
+        <h4>Kaniyama-t/SchoolInfoSys1</h4>
+        <a href="https://github.com/Kaniyama-t/SchoolInfoSys1/"><img alt=".github/workflows/main.yml" src="https://github.com/Kaniyama-t/SchoolInfoSys1/workflows/.github/workflows/main.yml/badge.svg" /><>
+    <hr />
+    <h3>ファイル一覧</h3>
     <?php
         $dir = "./";
         $filelist = glob($dir . '*.php');
@@ -10,7 +16,7 @@
         foreach ($filelist as $file) {
         
             if (is_file($file)) {
-                print("<a href=\"./".substr($file,2,0)."\">".substr($file,2,0)."</a>");
+                print("<a href=\"./".substr($file,2)."\">".substr($file,2)."</a>");
                 echo nl2br("\n");
             }
         }
