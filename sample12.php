077bd3ab8c451272c5efe611a6ee94e5437f52ca
Kaniyama_t <webmaster@kaniyama.net>
2020-05-08 12:04:08 +0900

    [FIX] sample12.phpで改行リテラルの置き換えを指定ない箇所を修正

--- sample12.php
+++ sample12.php
@@ -7,7 +7,7 @@
 <body>
     <?php 
         $filelist = `ls -la | grep '.php$'`; //ファイルの一覧コマンド
-        echo "<pre>¥n$filelist</pre>".PHP_EOL;
+        echo "<pre>".PHP_EOL."$filelist</pre>".PHP_EOL;
         $nitiji= `date`;
         echo "日付：$nitiji<br>".PHP_EOL;; 
     ?>
