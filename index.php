4a2e7afc2d4e4def28cf3e1baf1025aae9bfc97e
Kaniyama_t <webmaster@kaniyama.net>
2020-05-11 18:30:27 +0900

    Github API (commit) を使用し，最新のcommit情報を表示

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
     <h1>情シス1 授業課題デプロイ場所</h1>
     <hr />
     <h3>Github Repository</h3>
-        <h4>Kaniyama-t/SchoolInfoSys1</h4>
-        最終更新のアップロード状況：<a href="https://github.com/Kaniyama-t/SchoolInfoSys1/"><img alt=".github/workflows/main.yml" src="https://github.com/Kaniyama-t/SchoolInfoSys1/workflows/CI/badge.svg?<?php echo date("YmdHis");?>" /></a>
+        <h4><a href="https://github.com/Kaniyama-t/SchoolInfoSys1">Kaniyama-t/SchoolInfoSys1</a></h4>
+        最終更新のアップロード状況：<a href="https://github.com/Kaniyama-t/SchoolInfoSys1/"><img alt=".github/workflows/main.yml" src="https://github.com/Kaniyama-t/SchoolInfoSys1/workflows/CI/badge.svg?<?php echo date("YmdHis");?>" /></a><br />
+        <?php 
+            $url = "https://api.github.com/repos/Kaniyama-t/SchoolInfoSys1/commits";
+            $githubsrc = file_get_contents($url);
+            $githubsrc = mb_convert_encoding($githubsrc, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+            $array = json_decode( $githubsrc , true );
+            echo "最終更新：[".$array[0]['committer']['date']."]".$array[0]['commit']['message'];
+        ?>
     <hr />
     <h3>ファイル一覧</h3>
     <?php
