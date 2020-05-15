9126730f4c5daec024cdaa0906d9c9b7a18c7bf9
Kaniyama-t <e17266@kagawa.kosen-ac.jp>
2020-05-15 20:24:32 +0900

    2020/05/15演習 2

---
+++ subject02.php
@@ -0,0 +1,10 @@
+<?php
+    $value1 = 100;
+    $value2 = 50;
+    $sub=($value1-$value2);
+    if($sub == 0){
+        print("value1の値とvalue2の値は同じ数です．")
+    }else{
+        print("value1=".$value1."とvalue2=".$value2."の差は".."です．")
+    }
+?>
