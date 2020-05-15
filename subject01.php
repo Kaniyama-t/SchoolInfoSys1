5e5d3172b31b9247dc10f9b523fe282f766eda95
Kaniyama-t <e17266@kagawa.kosen-ac.jp>
2020-05-15 20:29:29 +0900

    2020/05/15演習 3

--- subject01.php
+++ subject01.php
@@ -1,17 +1,18 @@
 <?php
     function getYearSymbol($timestamp){
         if(strtotime("2020/05/01")<=$timestamp)
-            return "令和".(date("YY")-2020+1)."年";
+            return "令和".(date("Y")-2020+1)."年";
         elseif(strtotime("1989/01/08")<=$timestamp)
-            return "平成".(date("YY")-1989+1)."年";
+            return "平成".(date("Y")-1989+1)."年";
         elseif(strtotime("1926/12/25")<=$timestamp)
-            return "昭和".(date("YY")-1926+1)."年";
+            return "昭和".(date("Y")-1926+1)."年";
         elseif(strtotime("1912/07/30")<=$timestamp)
-            return "大正".(date("YY")-1912+1)."年";
+            return "大正".(date("Y")-1912+1)."年";
         elseif(strtotime("1868/10/23")<=$timestamp)
-            return "明治".(date("YY")-1868+1)."年";
+            return "明治".(date("Y")-1868+1)."年";
         else
             return "ERROR:不正な値";
+        var_dump(date("Y"));
     }
 
     $name="詫間太郎";
@@ -19,5 +20,5 @@
     $birthday=strtotime("2001/04/02");
     $birthyear_symbol=getYearSymbol($birthday);
 
-    print("私の名前は".$name."です。年齢は".$age."才、生年月日は".$birthyear_symbol.date("mm月dd日",$birthday)."です。");
+    print("私の名前は".$name."です。年齢は".$age."才、生年月日は".$birthyear_symbol.date("m月d日",$birthday)."です。");
 ?>
